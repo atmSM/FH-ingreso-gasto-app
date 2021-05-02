7b9888f962f4c8a336e4e365833cfb89f880eb06
SantiMS <santiago.martinez@atm-maggioli.es>
2021-05-02 19:15:01 +0200

    Primer commit - template listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IngresoGastoComponent } from './ingreso-gasto/ingreso-gasto.component';
+import { EstadisticaComponent } from './ingreso-gasto/estadistica/estadistica.component';
+import { DetalleComponent } from './ingreso-gasto/detalle/detalle.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    IngresoGastoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+import { EstadisticaComponent } from '../ingreso-gasto/estadistica/estadistica.component';
+import { IngresoGastoComponent } from '../ingreso-gasto/ingreso-gasto.component';
+import { DetalleComponent } from '../ingreso-gasto/detalle/detalle.component';
+
+export const dashboardRoutes: Routes = [
+  { path: '', component: EstadisticaComponent },
+  { path: 'ingreso-gasto', component: IngresoGastoComponent },
+  { path: 'detalle', component: DetalleComponent },
+]
